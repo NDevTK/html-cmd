e9b0406da80cf6f657f477438f7f7d06f3100fc5
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-06-29 01:35:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('fetch', function (event) {
 
 function inject(response) {
 const headers = new Headers(response.headers);
-headers.set('Content-Security-Policy', 'sandbox allow-scripts');
+headers.set('Content-Security-Policy', 'sandbox allow-scripts allow-popups');
 headers.set('X-Frame-Options', 'SAMEORIGIN');
 headers.set('Cross-Origin-Opener-Policy', 'same-origin');
 headers.set('Strict-Transport-Security', 'max-age=31536000');
